eaf3383f7d6ef6dd336f233bc71858d88e8a170e
Jeff Williams <jwill1258@gmail.com>
2020-03-30 00:54:20 -0400

    server update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require ("express");
+var path = require("path");
 var app = express();
 
-var PORT = proccess.env.PORT || 8080;
+var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("../app/routing/htmlRoutes")(app);
-require("../app/routing/apiRoutes")(app);
+require("../FriendFinder/app/routing/htmlRoutes")(app);
+require("../FriendFinder/app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
